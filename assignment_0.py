4445ad6a244cc87a78f383834a6a3d6bf7eba69c
Robert <rrb254@cornell.edu>
2026-09-09 20:09:39 -0400

    Switch assignment 0 to RK4 integrator

--- assignment_0.py
+++ assignment_0.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 from models import pendulum as model
 
+from integrators import rk4 as integrator
+
 # Basic simulation of the pendulum
 
 params = {
@@ -16,7 +18,7 @@ params = {
 # some set-up
 initial_state = np.array([np.pi / 4, 0.0])
 
-timestep = 1e-5
+timestep = 9.96e-2
 sim_time = 5.0
 
 n_timesteps = int(sim_time / timestep) + 1
@@ -26,15 +28,20 @@ state_traj[:, 0] = initial_state
 
 # simulation loop
 for step, t in enumerate(time_traj[:-1]):
-    state_traj[:, step + 1] = state_traj[:, step] + timestep * model.dynamics(
-        t, state_traj[:, step], params
-    )
+    state_traj[:, step + 1] = integrator(t, state_traj[:, step], timestep, model.dynamics, params)
 
 # sanity check the energies: since there is no actuation, and no damping, total energy should stay
 # constant. If we turn on the damping coefficient, it should slowly bleed out energy until it comes to
 # a stand-still.
 
-potential_energy, kinetic_energy = model.calculate_energy(state_traj, params)
+kinetic_energy, potential_energy = model.calculate_energy(state_traj, params)
+
+e_change = (kinetic_energy[-1] + potential_energy[-1]) - (kinetic_energy[0] + potential_energy[0])
+if (abs(e_change) <= 1.0e-3):
+    print("timestep is good")
+else:
+    print("timestep too large")
+
 
 plt.figure()
 plt.plot(time_traj, potential_energy, label="Potential energy")
